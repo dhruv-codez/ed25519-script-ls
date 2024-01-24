9de8d057797f4cc7cac4439eab50db10008e85cd
vc-dhruv <dhruv.prajapati@viitor.cloud>
2024-01-24 18:05:12 +0530

    feat: update logic for fetching the data

--- index.js
+++ index.js
@@ -2,14 +2,24 @@ const nacl = require('tweetnacl');
 const naclUtil = require('tweetnacl-util');
 
 exports.handler = async (event) => {
-  // Retrieve the message from the event object
-  const message = event.message || 'Hello EVRC!';
+  let body;
+  try {
+    // Retrieve the message from the event object
+    body = JSON.parse(event.body);
+  } catch (error) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ error: "Body is not valid JSON" }),
+    };
+  }
+
+  const message = JSON.stringify(body);
+  const messageUint8 = naclUtil.decodeUTF8(message);
 
   // Generate a new key pair
   const keyPair = nacl.sign.keyPair();
 
   // Sign a message
-  const messageUint8 = naclUtil.decodeUTF8(message);
   const signature = nacl.sign.detached(messageUint8, keyPair.secretKey);
 
   return {
